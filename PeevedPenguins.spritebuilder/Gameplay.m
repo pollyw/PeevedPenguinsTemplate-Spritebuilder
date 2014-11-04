d53e2f261627cfb32a65340249b272734db3d99b
Polly Wu <pollywu@pollys-air.gateway.sonic.net>
2014-11-03 17:34:17 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -23,6 +23,9 @@
     //load level 1
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
+    //visualize physics bodies & joints
+    _physicsNode.debugDraw = true;
 }
 
 //called on every touch in this scene
