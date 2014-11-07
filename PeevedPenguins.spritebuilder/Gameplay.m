947e4fae9bb317f5eca2bc8c7b63af40a6fc9d77
Polly Wu <pollywu@pollys-air.gateway.sonic.net>
2014-11-07 13:21:07 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -137,10 +137,6 @@
     [_contentNode runAction:follow];
 }
 
--(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
-    CCLOG(@"Something collided with a seal!");
-}
-
 -(void)retry {
     //relaod this level
     [[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"Gameplay"]];
