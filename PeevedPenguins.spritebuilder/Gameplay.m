c2e3a9bd3fd8a7a4f1c947388319ca6609a633cf
Polly Wu <pollywu@Pollys-MacBook-Air.local>
2014-11-08 16:33:39 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -166,6 +166,13 @@
     
     //finally, remove the destroyed seal
     [seal removeFromParent];
+    
+    //generate a random number between 0.0 and 2.0
+    float delay = (arc4random() % 2000) / 1000.f;
+    
+    //call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+
 }
 
 -(void)startBlnkAndJump {
