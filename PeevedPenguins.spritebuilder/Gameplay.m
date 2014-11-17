6188f78fd86cbbc003699fe5ff98db71af1d1654
Polly Wu <pollywu@pollys-air.gateway.sonic.net>
2014-11-17 13:49:27 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -131,7 +131,7 @@
     [_physicsNode addChild:penguin];
     
     //manually create & apply a force to launch the penguin
-    CGPoint launchDirection = ccp(1, 0);
+    CGPoint launchDirection = ccp(0,1);
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
     
