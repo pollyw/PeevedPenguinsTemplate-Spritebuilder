2b7d04e309c78926fdc653fea9683dcf2c5ec395
Polly Wu <pollywu@Pollys-MacBook-Air.local>
2014-11-30 21:48:13 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -171,7 +171,7 @@
     [penguin.physicsBody applyForce:force];
     
     
-    //ensure followed object is in visible are when starting
+    //ensure followed object is visible are when starting
     self.position = ccp(0,0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
     
